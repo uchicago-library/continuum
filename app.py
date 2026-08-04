9cff1c2eb15795ad87745944bfbb76bedccc753a
svirgilgood <svirgilgoodwin@gmail.com>
2026-08-04 14:19:29 -0600

    update the file requests to handle the pres and pdf arguments

--- app.py
+++ app.py
@@ -24,6 +24,56 @@ DB = os.getenv("CONTINUUMDB")
 store: TripleStore
 
 
+def construct_file_arguments(
+    ark_id: str,
+    file_name: Optional[str] = None,
+    page: Optional[str] = None,
+    version: Optional[str] = None,
+):
+    if file_name and file_name.startswith("000"):
+        page = file_name
+        file_name = None
+    if file_name and file_name.startswith("v"):
+        version = file_name
+        file_name = page
+        page = None
+
+    # mime_type = "application/pdf" if file_name == "pdf" else None
+    mime_type = None
+    # if mime_type:
+    #    file_name = None
+
+    if file_name:
+        fname, ext = os.path.splitext(file_name)
+        if "vaf" in fname:
+            # print(fname)
+            type_node = filter_file_types("viewer")
+            # print(type_node)
+        elif file_name == "pdf":
+            file_name = None
+            mime_type = "application/pdf"
+            type_node = filter_file_types("preservation")
+        elif file_name == "pres":
+            type_node = filter_file_types("preservation")
+            file_name = None
+        else:
+            type_node = filter_file_types(
+                "preservation" if ext in (".pdf", ".tif", ".wav") else "supplemental"
+            )
+    else:
+        type_node = filter_file_types("preservation")
+    obj = FileArguments(
+        ark_id=ark_id,
+        type_node=type_node,
+        version=version,
+        file_name=file_name,
+        page=page,
+        mime_type=mime_type,
+    )
+    # print("file arguments", obj)
+    return obj
+
+
 def create_app(test_config=None):
     """ """
     # Initialize the triple store
@@ -47,6 +97,24 @@ def create_app(test_config=None):
 
     """
 
+    """
+    Commenting out the file sizes
+    @app.route("/size/<ark_id>/<file_name>")
+    @app.route("/size/<ark_id>/<page>/<file_name>")
+    @app.route("/size/<ark_id>/<page>/<file_name>/<version>")
+    def get_size(
+        ark_id: str,
+        file_name: Optional[str] = None,
+        page: Optional[str] = None,
+        version="head",
+    ):
+        """ """
+        file_obj = construct_file_arguments(
+            ark_id, file_name=file_name, page=page, version=version
+        )
+        return "hello world"
+    """
+
     @app.route("/file/<ark_id>")
     @app.route("/file/<ark_id>/<file_name>")
     @app.route("/file/<ark_id>/<page>/<file_name>")
@@ -61,49 +129,18 @@ def create_app(test_config=None):
         Used by the ark Resolver to return the file, if the file is not passed, URLs for the
         different files are returned.
         argv0 : ark_id
-        argv1 : file_name | page | "pdf"
+        argv1 : file_name | page | "pdf" | "pres"
         argv2 : file_name | version
         argv3 : version
         <ark_id>/<page>/<file_name>/<version>
         """
         # print(f"ark_id: {ark_id}, file_name: {file_name}, version: {version}")
-        if file_name and file_name.startswith("000"):
-            page = file_name
-            file_name = None
-        if file_name and file_name.startswith("v"):
-            version = file_name
-            file_name = page
-            page = None
-
-        mime_type = "application/pdf" if file_name == "pdf" else None
-        if mime_type:
-            file_name = None
-
-        if file_name:
-            fname, ext = os.path.splitext(file_name)
-            if "vaf" in fname:
-                # print(fname)
-                type_node = filter_file_types("viewer")
-                # print(type_node)
-            else:
-                type_node = filter_file_types(
-                    "preservation"
-                    if ext in (".pdf", ".tif", ".wav")
-                    else "supplemental"
-                )
-        else:
-            type_node = filter_file_types("preservation")
-        obj = FileArguments(
-            ark_id=ark_id,
-            type_node=type_node,
-            version=version,
-            file_name=file_name,
-            page=page,
-            mime_type=mime_type,
+        obj = construct_file_arguments(
+            ark_id, file_name=file_name, page=page, version=version
         )
-        print("file arguments", obj)
+        # print("file arguments", obj)
         image_obj = store.find_file_path(obj)
-        print("image obj", image_obj)
+        # print("image obj", image_obj)
         if len(image_obj) == 1:
             image_path = Path(image_obj[0]["path"])
 
