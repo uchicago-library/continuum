92939421f04a6a1485e0e5a75fc76531b2a7f27a
svirgilgood <svirgilgoodwin@gmail.com>
2026-08-28 13:16:38 -0600

    Adding Queries for Frontend
    * Updates for the front end queries
    * These add many methods to the store object

--- src/continuum/triplestore.py
+++ src/continuum/triplestore.py
@@ -215,3 +215,286 @@ class TripleStore:
         if not isinstance(results, QuerySolutions):
             raise Exception("Error in query")
         return [{"ark": res["ark"].value, "path": res["path"].value} for res in results]
+
+    def get_collections(self):
+        print("hello?")
+        query = """
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+
+        SELECT ?collection
+        WHERE {
+        ?collectionNode
+            a continuum:Collection ;
+            rdfs:label ?collection ;
+            .
+        }
+        """
+        results = self.store.query(query)
+        return [{"Collection": res["collection"].value} for res in results]
+
+    def get_arks(self):
+        self.logger.debug("In Arks")
+        query = """
+        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+        PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+
+        SELECT ?ark
+        WHERE {
+            ?arkNode
+                a edm:ProvidedCHO ;
+                continuum:hasArkID ?ark ;
+            .
+        }
+        """
+        results = self.store.query(query)
+        return [
+            {"Ark": res["ark"].value.replace(PREFIXES["ark"], "")} for res in results
+        ]
+
+    def get_local_ids(self):
+        query = """
+
+        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+        PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+
+        SELECT ?localId
+        WHERE {
+            ?ark
+                a edm:ProvidedCHO ;
+                continuum:originalIdentifier ?localId ;
+            .
+        }
+        """
+        results = self.store.query(query)
+        return [{"local-id": res["localId"].value} for res in results]
+
+    def format_main_table_results(self, query: str):
+        """
+        Query needs to have the arguments 'collection', 'ark', and 'local-id'
+        provide the query and format it for the results"""
+        self.logger.debug("Format Main table Query: {query}")
+        results = self.store.query(query)
+        if not isinstance(results, QuerySolutions):
+            raise Exception(f"Error in Query {query}")
+        # print(results)
+        return [
+            {
+                "collection": res["collection"].value if res["collection"] else "",
+                "ark": res["ark"].value,
+                "local-id": res["local_id"].value,
+            }
+            for res in results
+        ]
+
+    def find_ark(self, ark_id: str):
+        # print("In find ark")
+        """
+        if ark_id.startswith("ark:61001/"):
+            ark_node = NamedNode(PREFIXES["ark"] + ark_id)
+        elif ark_id.startswith("ark:/61001/"):
+            ark_node
+        else:
+            ark_node = NamedNode(f"{PREFIXES['ark']}ark:61001/{ark_id}")
+        """
+        query = """
+        PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+        PREFIX ark: <http://ark.lib.uchicago.edu/ark:61001/>
+        SELECT ?collection ?ark ?local_id
+        WHERE{
+            VALUES ?ark { %s }
+            ?arkNode
+                a edm:ProvidedCHO ;
+                continuum:hasArkID ?ark ;
+                continuum:originalIdentifier ?local_id ;
+            .
+            OPTIONAL {
+              ?arkNode
+                dcterms:isPartOf/rdfs:label ?coll .
+            }
+            BIND(IF(BOUND(?coll),  "None", ?coll) AS ?collection)
+        }
+        """ % Literal(ark_id.replace("ark:61001/", "").replace("ark:/61001/", ""))
+        print(query)
+        return self.format_main_table_results(query)
+
+    def find_local_id(self, local_id: str):
+        """find an ark by the local id"""
+        local_node = Literal(local_id)
+        query = """
+        PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ark: <http://ark.lib.uchicago.edu/ark:61001/>
+        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+
+        SELECT ?collection ?ark ?local_id
+        WHERE{
+            VALUES ?local_id { %s }
+            ?arkNode
+                a edm:ProvidedCHO ;
+                continuum:originalIdentifier ?local_id ;
+                continuum:hasArkID ?ark ;
+                #dcterms:isPartOf/rdfs:label ?collection ;
+            .
+            OPTIONAL {
+              ?arkNode
+                dcterms:isPartOf/rdfs:label ?coll .
+            }
+            BIND(IF(BOUND(?coll),  "None", ?coll) AS ?collection)
+
+            #BIND(REPLACE(STR(?arkNode), str(ark:), "") AS ?ark)
+        }
+        """ % local_node
+        print(f"query: {query}")
+        return self.format_main_table_results(query)
+
+    def find_collection(self, collection: str):
+        """find all of the items in a collection"""
+        collection_node = Literal(collection)
+        query = """
+        PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ark: <http://ark.lib.uchicago.edu/ark:61001/>
+
+        SELECT ?collection ?ark ?local_id
+        WHERE {
+            VALUES ?collection { %s }
+            ?collectionNode
+                a continuum:Collection ;
+                rdfs:label ?collection ;
+                ^dcterms:isPartOf ?arkNode ;
+            .
+            ?arkNode
+                continuum:originalIdentifier ?local_id ;
+                continuum:hasArkID ?ark ;
+            .
+            #BIND(REPLACE(STR(?arkNode), str(ark:), "") AS ?ark)
+        }
+        """ % collection_node
+        # print(f"query: {query}")
+        return self.format_main_table_results(query)
+
+    def search_for_term(self, column: str, term: str):
+        """ """
+        print(f"in search for term: {column}:: {term}")
+        match column.lower():
+            case "ark":
+                return self.find_ark(term)
+            case "collection":
+                return self.find_collection(term)
+            case "local-id":
+                print(term)
+                return self.find_local_id(term)
+            case _:
+                return {"Error": f"No column named {column}, for term: {term}"}
+
+    def retrieve_file_data(self, ark_id: str):
+        """ """
+        file_query = """
+    PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+    SELECT ?file ?file_type ?path ?version ?created ?url
+    WHERE {
+    VALUES ?ark { %s }
+    ?arkNode
+            continuum:hasArkID ?ark .
+    ?file
+        continuum:fileType ?file_type ;
+        continuum:hasPath ?path ;
+        continuum:partOfVersion ?version ;
+        continuum:filename ?filename ;
+        dcterms:created ?created ;
+        dcterms:isPartOf ?arkNode ;
+        .
+        BIND(CONCAT("/file/", ?ark, "/", ?filename, "/", ?version) AS ?url)
+    }
+        """ % Literal(ark_id)
+
+        # print(file_query)
+
+        results = self.store.query(file_query)
+        if not isinstance(results, QuerySolutions):
+            raise Exception("Error In query")
+        res: Dict[str, List[Dict[str, str]]] = {}
+        for r in results:
+            version = r["version"].value
+            res.setdefault(version, []).append(
+                {
+                    "file_type": r["file_type"].value,
+                    "path": r["path"].value,
+                    "created": r["created"].value,
+                    "url": r["url"].value,
+                }
+            )
+        # print(res)
+        return res
+
+    def retrieve_ark_data(self, ark_id: str):
+        ark_query = """
+    PREFIX continuum: <https://continuum.lib.uchicago.edu/ontology/>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX ark: <http://ark.lib.uchicago.edu/>
+
+    SELECT
+        ?ark
+        ?head
+        (GROUP_CONCAT(DISTINCT STR(?local); separator="|") AS ?local_id)
+        (GROUP_CONCAT(DISTINCT STR(?coll); separator="|") AS ?collection)
+        (GROUP_CONCAT(DISTINCT STR(?modified); separator="|") AS ?mods)
+    WHERE {
+    VALUES ?arkId { %s }
+    ?ark
+        continuum:head ?head ;
+        continuum:hasArkID ?arkId ;
+        continuum:originalIdentifier ?local ;
+        dcterms:modified ?modified ;
+        .
+        OPTIONAL {
+        ?ark
+            dcterms:isPartOf/rdfs:label ?coll ;
+        .
+        }
+    }
+    GROUP BY ?ark ?head
+        """ % Literal(ark_id)
+        self.logger.debug(f"ark_query: {ark_query}")
+
+        results = self.store.query(ark_query)
+        # self.logger.debug([r for r in results])
+        if not isinstance(results, QuerySolutions):
+            raise Exception("Error in query")
+        return [
+            {
+                "ark": r["ark"].value,
+                "head": r["head"].value,
+                "local_id": r["local_id"].value.split("|"),
+                "collection": (
+                    r["collection"].value.split("|") if r["collection"] else ""
+                ),
+                "modified": r["mods"].value.split("|"),
+            }
+            for r in results
+        ]
+
+    def retrieve_object_data(
+        self,
+        ark_id: str,
+    ) -> Dict[str, str]:
+        """
+        Grab all of the Information about an Object
+        """
+
+        ark_object = self.retrieve_ark_data(ark_id)
+        self.logger.debug(f"ark object:  {ark_object}")
+        file_object = self.retrieve_file_data(ark_id)
+        self.logger.debug(f"file_object: {file_object}")
+
+        return {"ark": ark_object, "file": file_object}
