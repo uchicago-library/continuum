ed2b4e2b4cbbdac098a2d0b675aacc39991932b4
svirgilgood <svirgilgoodwin@gmail.com>
2026-09-09 14:19:50 -0600

    update the test to handle paths

--- conftest.py
+++ conftest.py
@@ -1,12 +1,34 @@
 import pytest
 
 from continuum.app import create_app
+from pathlib import Path
+
+"""
+BASEDIR = os.getenv("BASEDIR")
+
+# BASEDIR = "/data/digital_collections_ocfl/ark_data/"
+# DB = Path("/data/local/app_data/project.db")
+DB = os.getenv("CONTINUUMDB")
+#
+#
+TURTLE_FILE = os.getenv("CONTINUUM_TURTLE")
+#
+"""
+
+BASEDIR = Path(__file__).parent / "tests/"
+DB_PATH = Path(__file__).parent / "tests" / "test.db"
+TURTLE_FILE = Path(__file__).parent / "tests" / "test_data.ttl"
 
 
+# db_path=DB, basedir=BASEDIR, turtle_file=TURTLE_FILE
 @pytest.fixture()
 def app():
-    app = create_app()
-    app.config.update({"TESTIG": True})
+    app = create_app(
+        basedir=BASEDIR,
+        db_path=DB_PATH,
+        turtle_file=TURTLE_FILE,
+    )
+    app.config.update({"TESTING": True})
 
     yield app
 
