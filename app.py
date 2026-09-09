9030780de4571708a4627d90ea8b5cc7c38bd54b
svirgilgood <svirgilgoodwin@gmail.com>
2026-09-09 14:16:44 -0600

    update the app to handle the passing in of environmental variables

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from src.continuum.app import create_app
+from dotenv import load_dotenv
+import os
 
-# Environmental  Configs
+load_dotenv()
 
-app = create_app()  # pass the system variables
+BASEDIR = os.getenv("BASEDIR")
+DB = os.getenv("CONTINUUMDB")
+TURTLE_FILE = os.getenv("CONTINUUM_TURTLE")
+
+app = create_app(db_path=DB, basedir=BASEDIR, turtle_file=TURTLE_FILE)
