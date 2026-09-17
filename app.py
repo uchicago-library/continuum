b34420b872ad6536cc3e53f051407b3c991b156b
svirgilgood <svirgilgoodwin@gmail.com>
2026-09-17 07:02:56 -0600

    adding the INDEX_DB to app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src.continuum.app import create_app
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 load_dotenv()
@@ -7,5 +8,8 @@ load_dotenv()
 BASEDIR = os.getenv("BASEDIR")
 DB = os.getenv("CONTINUUMDB")
 TURTLE_FILE = os.getenv("CONTINUUM_TURTLE")
+INDEX_DB = Path(DB).parent / "index.db"  # os.getenv("INDEX_DB")
 
-app = create_app(db_path=DB, basedir=BASEDIR, turtle_file=TURTLE_FILE)
+app = create_app(
+    db_path=DB, basedir=BASEDIR, turtle_file=TURTLE_FILE, index_db=INDEX_DB
+)
